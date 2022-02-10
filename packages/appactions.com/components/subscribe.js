b187270758adfcc7d60936cada4f028d21044be0
Gabor <altosun@protonmail.com>
2022-02-10 09:51:27 +0100

    Styling

--- packages/appactions.com/components/subscribe.js
+++ packages/appactions.com/components/subscribe.js
@@ -1,13 +1,9 @@
 import SignUp from './signup';
 
 const Subscribe = () => (
-    <section className="relative mb-16">
-        <div
-            className="absolute left-0 right-0 h-1/2 bg-warm-gray-50"
-            aria-hidden="true"
-        />
-        <div className="relative px-4 mx-auto max-w-7xl lg:px-8">
-            <div className="px-6 py-10 rounded-lg bg-gradient-to-l from-teal-400 to-brand-green sm:py-16 sm:px-12 lg:py-20 lg:px-20 lg:flex lg:items-center">
+    <section>
+        <div className="px-4 mx-auto max-w-7xl lg:px-8 mb-16">
+            <div className="px-6 py-10 rounded-lg bg-gradient-to-l from-teal-400 to-brand-green sm:py-16 sm:px-12 lg:flex lg:items-center">
                 <div className="lg:w-0 lg:flex-1">
                     <h2 className="text-3xl font-extrabold tracking-tight text-white">
                         Sounds interesting?
@@ -17,7 +13,8 @@ const Subscribe = () => (
                         features.
                     </p>
                 </div>
-                <div className="mt-12 sm:w-full sm:max-w-md lg:mt-0 lg:ml-8 lg:flex-1">
+
+                <div className="mt-12 sm:w-full sm:max-w-md lg:mt-0 lg:ml-8 sm:flex-1">
                     <SignUp label="Subscibe" />
                 </div>
             </div>
