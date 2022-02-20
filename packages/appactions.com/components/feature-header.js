ec61ebff64a05854de65c256d6f209609a1c8b75
Miklos Megyes <m.miklos05@gmail.com>
2022-02-21 00:56:54 +0100

    fix colors

--- packages/appactions.com/components/feature-header.js
+++ packages/appactions.com/components/feature-header.js
@@ -6,7 +6,7 @@ const FeatureHeader = ({ icon, title, ctaHref }) => (
     <div className="flex flex-col sm:items-center lg:items-start lg:w-1/2">
         <div className="flex flex-row items-center sm:flex-col text-gray-800 lg:items-baseline">
             {icon}
-            <h3 className="gotham font-medium leading-tight text-3xl sm:text-4xl sm:pt-10 sm:text-center lg:text-left">
+            <h3 className="font-medium leading-tight text-3xl sm:text-4xl sm:pt-10 sm:text-center lg:text-left">
                 {title}
             </h3>
         </div>
@@ -15,7 +15,7 @@ const FeatureHeader = ({ icon, title, ctaHref }) => (
 
         <div className="mt-4 sm:mt-12">
             {/* <Link href={ctaHref}>
-                <a className="hidden sm:block py-3 px-4 text-center whitespace-nowrap flex items-center font-medium leading-6 gotham rounded-md transition duration-150 ease-in-out sm:flex-shrink-0 inline-flex items-center sm:w-auto focus:outline-none focus:ring bg-brand-green text-lg text-white ring-green-400 hover:opacity-80">
+                <a className="hidden sm:block py-3 px-4 text-center whitespace-nowrap flex items-center font-medium leading-6 rounded-md transition duration-150 ease-in-out sm:flex-shrink-0 inline-flex items-center sm:w-auto focus:outline-none focus:ring bg-brand-green text-lg text-white ring-green-400 hover:opacity-80">
                     Learn more <CaretRight className="ml-2 inline" />
                 </a>
             </Link> */}
